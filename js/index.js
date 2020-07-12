98d3c301f6c0c39cc6b38f80fd7df3e6c1693e65
Daria <dasha_kravchenko_00@mail.ru>
2020-07-12 15:04:16 +0300

    На защиту - вторая попытка

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 // слайдер
 let indicators = document.querySelectorAll(".indicator");
 
-function showSlides(number, bodyColor, blurColor) {
+function showSlides(number, bodyColor, blurColor, sliderImage) {
   let slides = document.querySelectorAll(".slider-element");
   let body = document.querySelector("body");
   let containerSlider = document.querySelector(".container-slider");
@@ -12,16 +12,16 @@ function showSlides(number, bodyColor, blurColor) {
     slides[i].style.display = "none";
     indicators[i].classList.remove("indicator-current");
   }
-
+  slides[number - 1].dataset.sliderImage = sliderImage;
   containerSlider.dataset.blurColor = blurColor;
   body.style.backgroundColor = "var(--" + bodyColor + ")";
-  slides[number - 1].style.display = "block";
+  slides[number - 1].style.display = "flex";
   indicators[number - 1].classList.add("indicator-current");
 }
 
 for (let i = 0; i < indicators.length; i++) {
   indicators[i].addEventListener("click", function() {
-    showSlides(i + 1, indicators[i].dataset.bodyColor, indicators[i].dataset.blurColor);
+    showSlides(i + 1, indicators[i].dataset.bodyColor, indicators[i].dataset.blurColor, indicators[i].dataset.sliderImage);
   });
 }
 
